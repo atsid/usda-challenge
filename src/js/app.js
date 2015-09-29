74456c75dbd36fbfd6085bad0cc471e4fb30fdc5
Nathan Evans <github@talkswithnumbers.com>
2015-09-29 15:33:00 -0700

    Add maps api

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,14 @@
     'use strict';
 
     console.log('js loaded');
+
+    var usda = {};
+
+    usda.map = new google.maps.Map($('#map')[0], {
+        center: { lat: 42, lng: -94 },
+        zoom: 4
+    });
+
+    global.usda = usda;
+
 }(this));
