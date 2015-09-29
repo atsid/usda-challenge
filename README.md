66eccc1b4be454f46fefa7f4994684e6e0246431
Nathan Evans <github@talkswithnumbers.com>
2015-09-29 14:37:53 -0700

    Project setup with build/deploy

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # usda-challenge
 ATS + EchoUser submission for USDA Innovation Challenge
+
+# Getting Started
+
+The app uses [gulp](http://gulpjs.com/) to do some builds and run a static server to host the content. To get started:
+
+1. Install [Node.js](https://nodejs.org/) if you don't already have it
+1. Clone the git repo to a user-owned directory where you have permissions
+1. Get dev dependencies: `npm install`
+1. Run code quality checks: `npm test`
+1. Run the app: `npm start`
+
+This will serve up the static content at http://localhost:8000/usda-challenge, *and* watch for file changes to automatically refresh itself.
+
+Optional: install gulp so you can execute it directly on the command line: `npm install -g gulp`.
+This is optional because `npm test` and `npm start` will get you running, and they map to a local gulp from node_modules.
+
---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var gulp = require('gulp');
+var path = require('path');
+var webserver = require('gulp-webserver');
+var jshint = require('gulp-jshint');
+var rename = require('gulp-rename');
+var rimraf = require('rimraf');
+var runSequence = require('run-sequence');
+var ghPages = require('gulp-gh-pages');
+
+var config = {
+  src: {
+    all: 'src',
+    siteFiles: 'src/**/*',
+    build: 'dist',
+    deploy: 'dist/src/**/*',
+    js: 'src/js/**/*.js'
+  }
+};
+
+/*
+ * jshint to check for syntax errors
+ */
+gulp.task('lint', function () {
+  return gulp.src(config.src.js)
+      .pipe(jshint({lookup: true}))
+      .pipe(jshint.reporter('default'));
+});
+
+/*
+ * Clean out the build directory so we don't have any excess junk
+ */
+gulp.task('clean', function (cb) {
+  rimraf(config.src.build, cb);
+});
+
+/*
+ * Copy static content into a single point for deployment, without the extra cruft.
+ */
+gulp.task('site', function () {
+  return gulp.src(config.src.siteFiles, { 'base': '.' }).pipe(gulp.dest(config.src.build));
+});
+
+/*
+ * Runs all the required tasks to create distributable site package in output folder.
+ */
+gulp.task('build', function (cb) {
+  return runSequence(
+      'site',
+      cb);
+});
+
+//helper for the web server task
+function serve(reload) {
+  return webserver({
+    path: '/usda-challenge',
+    livereload: reload,
+    defaultFile: 'index.html',
+    open: false
+  });
+}
+
+/*
+ * Live-reload server to make the app available (localhost:8000) and auto-refresh when files change.
+ */
+gulp.task('serve', function() {
+  gulp.src(config.src.all)
+      .pipe(serve(true));
+});
+
+/*
+ * Web server to host the app, but from output folder, replicating live deploy with built resources.
+ */
+gulp.task('serve-dist', function() {
+  gulp.src(config.src.build)
+      .pipe(serve(false));
+});
+
+/*
+ * Push the built site content to public gh-pages.
+ */
+gulp.task('ghpages', function () {
+  return gulp.src(config.src.deploy)
+      .pipe(ghPages());
+});
+
+/**
+ * Full deploy cycle.
+ */
+gulp.task('deploy', function (cb) {
+  runSequence(
+      'clean',
+      'build',
+      'ghpages',
+      cb);
+});
+
+gulp.task('default', ['lint', 'serve']);
