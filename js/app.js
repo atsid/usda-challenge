aacbbaea22cb164731dc334a841d9d305f22eacb
Travis-CI <github@atsid.com>
2015-09-29 22:54:58 +0000

    Update 2015-09-29T22:54:57.021Z

--- js/app.js
+++
@@ -1,5 +0,0 @@
-(function (global) {
-    'use strict';
-
-    console.log('js loaded');
-}(this));
---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+(function (global) {
+    'use strict';
+
+    console.log('js loaded');
+
+    var usda = {};
+
+    usda.map = new google.maps.Map($('#map')[0], {
+        center: { lat: 42, lng: -94 },
+        zoom: 4
+    });
+
+    global.usda = usda;
+
+}(this));
