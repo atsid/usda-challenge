685fae2e990cb81f418ae8ff84800b172b667e55
Nathan Evans <github@talkswithnumbers.com>
2015-10-05 08:30:46 -0700

    Add dashboard with temperature data

---
+++ src/js/Dashboard.js
@@ -0,0 +1,183 @@
+(function (global) {
+
+    var Dashboard = function (dataUrl) {
+
+        //setup dc
+        var stationsChart = dc.rowChart("#stations-chart");
+        var dateChart = dc.lineChart("#date-chart");
+        var tempChart = dc.compositeChart("#temp-chart");
+        var dataTable = dc.dataTable("#data-table");
+
+        global.stationsChart = stationsChart;
+        global.dateChart = dateChart;
+        global.tempChart = tempChart;
+        global.dataTable = dataTable;
+
+        d3.csv(dataUrl, function(err, data) {
+
+            var dateFormat = d3.time.format("%m/%d/%y");
+            var numeric = ['lat', 'lon', 'high', 'low', 'elevation'];
+            var nameKeys = {}; //map these to IDs for labeling later
+            //todo: is there a d3 map function for value coercion?
+            data.forEach(function(d) {
+                d.date = dateFormat.parse(d.date);
+                numeric.forEach(function (key) {
+                    d[key] = parseFloat(d[key]);
+                });
+                d.location = {
+                    latitude: d.lat,
+                    longitude: d.lon
+                };
+                nameKeys[d.id] = d.name;
+            });
+
+
+            var index = crossfilter(data);
+            var all = index.groupAll();
+
+            var stations = index.dimension(function(d) { return d.id; });
+            var dates = index.dimension(function(d) { return d3.time.day(d.date); });
+            var locations = index.dimension(function(d) { return d.location; });
+
+            var startDate = new Date('2015-01-01');
+            var endDate = new Date('2015-12-31');
+
+            var dateGroup = dates.group();
+            var stationGroup = stations.group();
+
+            var locationsGroup = stations.group().reduce(
+                function (p, v) {
+                    ++p.count;
+                    p.latitude = v.location.latitude;
+                    p.longitude = v.location.longitude;
+                    return p;
+                },
+                function (p, v) {
+                    --p.count;
+                    p.latitude = v.location.latitude;
+                    p.longitude = v.location.longitude;
+                    return p;
+                },
+                function () {
+                    return {count: 0};
+                }
+            );
+
+            var highGroup = dates.group().reduce(
+                function (p, v) {
+                    ++p.count;
+                    p.sum += v.high;
+                    p.avg = Math.round(p.sum / p.count);
+                    return p;
+                },
+                function (p, v) {
+                    --p.count;
+                    p.sum -= v.high;
+                    p.avg = p.count ? Math.round(p.sum / p.count) : 0;
+                    return p;
+                },
+                function () {
+                    return {count: 0, sum: 0, avg: 0};
+                }
+            );
+            var lowGroup = dates.group().reduce(
+                function (p, v) {
+                    ++p.count;
+                    p.sum += v.low;
+                    p.avg = Math.round(p.sum / p.count);
+                    return p;
+                },
+                function (p, v) {
+                    --p.count;
+                    p.sum -= v.low;
+                    p.avg = p.count ? Math.round(p.sum / p.count) : 0;
+                    return p;
+                },
+                function () {
+                    return {count: 0, sum: 0, avg: 0};
+                }
+            );
+
+
+            stationsChart
+                .width($('#stations-chart').innerWidth()-30)
+                .height(200)
+                .margins({top: 10, left:5, right: 10, bottom:20})
+                .colors(['#339966'])
+                .group(stations.group())
+                .dimension(stations)
+                .label(function (p) {
+                    return nameKeys[p.key];
+                })
+                .elasticX(true)
+                .gap(2)
+                .ordering(function(i){return -i.value;})
+                .labelOffsetY(8)
+                .xAxis().ticks(3);
+
+            stationsChart.on("postRedraw", function (e) {
+                console.log(locations.top(Infinity));
+                console.log(locations.top(Infinity).length);
+                console.log(locationsGroup.top(Infinity));
+            });
+
+            dateChart
+                .width($('#date-chart').innerWidth()-30)
+                .height(200)
+                .margins({top: 10, left:30, right: 10, bottom:20})
+                .x(d3.time.scale().domain([startDate, endDate]))
+                .colors(['#339966'])
+                .dimension(dates)
+                .group(dateGroup)
+                .renderArea(true);
+
+
+            //composite chart - set shared values, then use `compose` to add individual graphs
+            tempChart
+                .width($('#temp-chart').innerWidth()-30)
+                .height(200)
+                .margins({top: 10, left:30, right: 10, bottom:20})
+                .x(d3.time.scale().domain([startDate, endDate]))
+                .dimension(dates)
+                .compose([
+                    dc.lineChart(tempChart)
+                        .group(highGroup)
+                        .colors(['orange'])
+                        .valueAccessor(function (d) {
+                            return d.value.avg;
+                        }),
+                    dc.lineChart(tempChart)
+                        .group(lowGroup)
+                        .colors(['blue'])
+                        .valueAccessor(function (d) {
+                            return d.value.avg;
+                        })
+                ]);
+
+
+            dataTable
+                .dimension(dates)
+                .group(function (d) {
+                    return d.name;
+                })
+                .size(100) // (optional) max number of records to be shown, :default = 25
+                .columns([
+                    function(d) { return d.date; },
+                    function(d) { return d.high; },
+                    function(d) { return d.low; },
+                    function(d) { return d.lat; },
+                    function(d) { return d.lon; },
+                    function(d) { return d.elevation; }
+                ])
+                .sortBy( function(d) { return d.date; })
+                .order(d3.descending);
+
+            dc.renderAll();
+
+        });
+
+    };
+
+    global.usda.Dashboard = Dashboard;
+
+}(this));
--- src/js/app.js
+++ src/js/app.js
@@ -1,76 +1,14 @@
 (function (global) {
     'use strict';
 
-    console.log('js loaded');
+    var usda = global.usda;
+    var url = 'data/iowa-hi-lo-clipped.csv';
 
-    var usda = {};
-
-    var map = new google.maps.Map($('#map')[0], {
-        center: { lat: 42, lng: -94 },
-        zoom: 5
-    });
-
-
-    //supports map search
-    var markers = [];
-    var input = document.getElementById('pac-input');
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    var searchBox = new google.maps.places.SearchBox(input);
-
-    google.maps.event.addListener(searchBox, 'places_changed', function () {
-        var places = searchBox.getPlaces();
-
-        if (places.length === 0) {
-            return;
-        }
-
-        for (var i = 0, m, l = markers.length; i < l; i++) {
-            m = markers[i];
-            m.setMap(null);
-        }
-
-        // For each place, get the icon, place name, and location.
-        markers = [];
-        var bounds = new google.maps.LatLngBounds();
-        for (var j = 0, place, l2 = places.length; j < l2; j++) {
-            place = places[j];
-            var image = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            var marker = new google.maps.Marker({
-                map: map,
-                icon: image,
-                title: place.name,
-                position: place.geometry.location
-            });
-
-            markers.push(marker);
-
-            bounds.extend(place.geometry.location);
-        }
-
-        // center the map on the search result
-        if (places.length === 1) {
-            map.setCenter(places[0].geometry.location);
-            //this should be more dynamic
-            map.setZoom(8);
-        }
-    });
-
-    google.maps.event.addListener(map, 'bounds_changed', function () {
-        var bounds = map.getBounds();
-        searchBox.setBounds(bounds);
-    });
-
-
-
+    var map = new usda.Map();
     usda.map = map;
-    global.usda = usda;
+
+    var dashboard = new usda.Dashboard(url);
+    usda.dashboard = dashboard;
+
 
 }(this));
