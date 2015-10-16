744fe1bfefa8c3784d86bc02c8e20a52ae5d97f0
Chris Trevino <chris.trevino@atsid.com>
2015-10-15 21:27:19 -0700

    #17 - Use react-google-maps
    Fixes #17

--- src/js/components/panes/map/map.js
+++ src/js/components/panes/map/map.js
@@ -2,44 +2,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import debugFactory from "debug";
+import GoogleMap from "react-google-maps/lib/GoogleMap";
 const debug = debugFactory('app:components:Map');
+
+const DEFAULT_ZOOM = 7;
+const DEFAULT_CENTER = {lat: 42, lng: -94};
 
 let MapComponent = React.createClass({
     getInitialState() {
         return {
-            initialCenter: {lat: 42, lng: -94},
-            initialZoom: 7,
-        };
-    },
-
-    componentDidMount () {
-        const mapNode = ReactDOM.findDOMNode(this.refs.map);
-        const map = new google.maps.Map($(mapNode)[0], {
-            center: this.state.initialCenter,
-            zoom: this.state.initialZoom,
-        });
-        this.map = map;
+            center: DEFAULT_CENTER,
+            zoom: DEFAULT_ZOOM,
+        }
     },
 
     setCenter(center) {
-        if (this.map && center) {
+        if (center) {
             debug('received new center', center);
-            this.map.setCenter(center);
+            this.setState({center});
         }
     },
 
     setBounds(bounds) {
-        if (this.map && bounds) {
+        if (bounds) {
             debug('received new bounds', bounds, this.map);
             const gbounds = new google.maps.LatLngBounds(bounds.sw, bounds.ne);
-            this.map.fitBounds(gbounds);
+            this.refs.gmap.fitBounds(gbounds);
         }
     },
 
     render() {
         return (
             <div className="mapContainer">
-                <div ref="map" className="map"></div>
+                <GoogleMap ref="gmap" containerProps={{ style: { height: "100%", minHeight: "500px" }}}
+                           defaultZoom={DEFAULT_ZOOM}
+                           defaultCenter={DEFAULT_CENTER}
+                           center={this.state.center}
+                           zoom={this.state.zoom}>
+                </GoogleMap>
             </div>
         );
     }
