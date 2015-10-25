ef4267927c2c03be32422bfe2960f0a69d8e1970
Travis-CI <github@atsid.com>
2015-10-25 06:45:49 +0000

    Update 2015-10-25T06:45:45.858Z

--- build/bundle.js
+++ build/bundle.js
@@ -57705,7 +57705,7 @@
 	    },
 	
 	    getInitialState: function getInitialState() {
-	        return { crops: [], selectedCrop: {} };
+	        return { crops: [], selectedCrop: { name: 'corn' } };
 	    },
 	
 	    componentDidMount: function componentDidMount() {
@@ -57719,7 +57719,7 @@
 	    loadCrops: function loadCrops(state) {
 	        var _this = this;
 	
-	        cropStore.getCrops().then(function (crops) {
+	        cropStore.getCropsByState(state).then(function (crops) {
 	            _this.setState({ crops: crops, state: state });
 	        });
 	    },
@@ -57789,6 +57789,59 @@
 	  wheat: 'wheat'
 	};
 	
+	var stateToCrops = {
+	  'AK': ['barley', 'hay', 'haylage', 'oats', 'wheat'],
+	  'AL': ['barley', 'beans', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'sorghum', 'soybeans', 'tobacco', 'wheat'],
+	  'AR': ['barley', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'rice', 'sorghum', 'soybeans', 'wheat'],
+	  'AZ': ['barley', 'beans', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'sorghum', 'wheat'],
+	  'CA': ['barley', 'beans', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'rice', 'sorghum', 'soybeans', 'sugarbeets', 'wheat'],
+	  'CO': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'sugarbeets', 'wheat'],
+	  'CT': ['beans', 'corn', 'hay', 'haylage', 'oats', 'soybeans', 'tobacco', 'wheat'],
+	  'DE': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'wheat'],
+	  'FL': ['beans', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'rice', 'sorghum', 'soybeans', 'sugarcane', 'tobacco', 'wheat'],
+	  'GA': ['barley', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'sorghum', 'soybeans', 'wheat'],
+	  'HI': ['corn', 'hay', 'haylage', 'sorghum', 'soybeans', 'sugarcane'],
+	  'IA': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'wheat'],
+	  'ID': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'sugarbeets', 'wheat'],
+	  'IL': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'rice', 'sorghum', 'soybeans', 'tobacco', 'wheat'],
+	  'IN': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'tobacco', 'wheat'],
+	  'KS': ['barley', 'beans', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'sugarbeets', 'tobacco', 'wheat'],
+	  'KY': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'peanuts', 'sorghum', 'soybeans', 'wheat'],
+	  'LA': ['beans', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'rice', 'sorghum', 'soybeans', 'sugarcane', 'tobacco', 'wheat'],
+	  'MA': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'soybeans', 'tobacco', 'wheat'],
+	  'MD': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'peanuts', 'sorghum', 'soybeans', 'tobacco', 'wheat'],
+	  'ME': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'tobacco', 'wheat'],
+	  'MI': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'sugarbeets', 'wheat'],
+	  'MN': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'sugarbeets', 'wheat'],
+	  'MO': ['barley', 'beans', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'rice', 'sorghum', 'soybeans', 'tobacco', 'wheat'],
+	  'MS': ['beans', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'rice', 'sorghum', 'soybeans', 'wheat'],
+	  'MT': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'soybeans', 'sugarbeets', 'wheat'],
+	  'NC': ['barley', 'beans', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'sorghum', 'soybeans', 'wheat'],
+	  'ND': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'sugarbeets', 'wheat'],
+	  'NE': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'sugarbeets', 'wheat'],
+	  'NH': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'wheat'],
+	  'NJ': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'wheat'],
+	  'NM': ['barley', 'beans', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'wheat'],
+	  'NV': ['barley', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'wheat'],
+	  'NY': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'tobacco', 'wheat'],
+	  'OH': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'sugarbeets', 'tobacco', 'wheat'],
+	  'OK': ['barley', 'beans', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'rice', 'sorghum', 'soybeans', 'wheat'],
+	  'OR': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'sugarbeets', 'wheat'],
+	  'PA': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'tobacco', 'wheat'],
+	  'RI': ['corn', 'hay', 'haylage', 'oats', 'soybeans', 'wheat'],
+	  'SC': ['barley', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'rice', 'sorghum', 'soybeans', 'tobacco', 'wheat'],
+	  'SD': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'sugarbeets', 'wheat'],
+	  'TN': ['barley', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'rice', 'sorghum', 'soybeans', 'tobacco', 'wheat'],
+	  'TX': ['barley', 'beans', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'rice', 'sorghum', 'soybeans', 'sugarcane', 'wheat'],
+	  'UT': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'sugarbeets', 'wheat'],
+	  'VA': ['barley', 'beans', 'corn', 'cotton', 'hay', 'haylage', 'oats', 'peanuts', 'sorghum', 'soybeans', 'wheat'],
+	  'VT': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'sugarbeets', 'wheat'],
+	  'WA': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'sugarbeets', 'wheat'],
+	  'WI': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'tobacco', 'wheat'],
+	  'WV': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'tobacco', 'wheat'],
+	  'WY': ['barley', 'beans', 'corn', 'hay', 'haylage', 'oats', 'sorghum', 'soybeans', 'sugarbeets', 'wheat']
+	};
+	
 	var CropStore = (function () {
 	  function CropStore() {
 	    _classCallCheck(this, CropStore);
@@ -57810,6 +57863,24 @@
 	
 	      return new Promise(function (resolve, reject) {
 	        resolve(_this.crops);
+	      });
+	    }
+	  }, {
+	    key: 'getCropsByState',
+	    value: function getCropsByState(state) {
+	      var _this2 = this;
+	
+	      return new Promise(function (resolve, reject) {
+	        var stateCrops = stateToCrops[state];
+	        var croplen = _this2.crops.length;
+	        var retval = [];
+	        for (var i = 0; i < croplen; ++i) {
+	          var c = _this2.crops[i];
+	          if (-1 !== stateCrops.indexOf(c.name)) {
+	            retval.push(c);
+	          }
+	        }
+	        resolve(retval);
 	      });
 	    }
 	
